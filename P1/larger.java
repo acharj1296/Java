131897217d00e5664d3dc6f6ca44f0fc7fe7bae0
acharj1296 <arjunacharya262@gmail.com>
2025-12-18 23:46:41 +0530

    Add files via upload

---
+++ P1/larger.java
@@ -0,0 +1,13 @@
+class larger {
+    public static void main(String args[]) {
+        int n1 = 5;
+        int n2 = 10;
+        if (n1 > n2) {
+            System.out.println(n1 + " is larger");
+        } else if (n2 > n1) {
+            System.out.println(n2 + " is larger");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+    }
+}
