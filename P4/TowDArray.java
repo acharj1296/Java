e88fea069ad053cc70deab5ffcb4b4ea0829646f
acharj1296 <arjunacharya262@gmail.com>
2026-01-05 09:10:04 +0530

    Refactor TowDArray to use predefined 2D array
    Replaced user input with a predefined 2D array and updated the display logic.

--- P4/TowDArray.java
+++ P4/TowDArray.java
@@ -1,25 +1,17 @@
-import java.util.Scanner;
 class TowDArray {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int rows = 3;
-        int cols = 4;
-        int[][] array = new int[rows][cols];
-        // Input elements into the 2D array
-        System.out.println("Enter " + (rows * cols) + " elements:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                array[i][j] = scanner.nextInt();
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Print the 2D array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
             }
+            System.out.println();
         }
-        // Display the elements of the 2D array
-        System.out.println("The elements in the 2D array are:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.println("Array[" + i + "][" + j + "] = " + array[i][j]);
-            }
-        }
-        scanner.close();
-        
     }
 }
